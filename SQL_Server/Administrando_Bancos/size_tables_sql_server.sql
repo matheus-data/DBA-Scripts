f9b620866b19e80bca8377b5f5f469c031e4dfaf
Matheus Nunes Rossi <126213415+matheus-data@users.noreply.github.com>
2026-07-09 18:17:39 -0300

    size_tables_sql_server
    Ele mostra:
    
    ✅ db: nome do banco atual.
    ✅ sche: schema da tabela.
    ✅ tb: nome da tabela.
    ✅ rows: quantidade de linhas.
    ✅ TotalSpaceGB: espaço total usado pela tabela em GB.

---
+++ SQL_Server/Administrando_Bancos/size_tables_sql_server.sql
@@ -0,0 +1,28 @@
+USE BASE_DADOS
+GO
+  
+SELECT  
+    DB_NAME() AS db,
+    s.name AS sche,
+    t.name AS tb,
+     p.rows,
+    CAST(ROUND(((SUM(a.total_pages) * 8) / 1024.00/ 1024.00), 2) AS NUMERIC(36, 2)) AS TotalSpaceGB
+    
+FROM 
+    sys.tables t
+INNER JOIN      
+    sys.indexes i ON t.object_id = i.object_id
+INNER JOIN 
+    sys.partitions p ON i.object_id = p.object_id AND i.index_id = p.index_id
+INNER JOIN 
+    sys.allocation_units a ON p.partition_id = a.container_id
+LEFT OUTER JOIN 
+    sys.schemas s ON t.schema_id = s.schema_id
+WHERE 
+    t.name NOT LIKE 'dt%' 
+    AND t.is_ms_shipped = 0
+    AND i.object_id > 255 
+GROUP BY 
+    t.name, s.name, p.rows
+ORDER BY 
+    TotalSpaceGB DESC, t.name
