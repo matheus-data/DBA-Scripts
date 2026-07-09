090a1a237bc04b32f4d0a4a95abbdaeb906c1c9d
Matheus Nunes Rossi <126213415+matheus-data@users.noreply.github.com>
2026-07-09 18:10:05 -0300

    Script to analyze filegroup space usage
    Ele:
    ✅ Mostra quanto espaço cada tabela/índice ocupa
    ✅ Agrupa por filegroup
    ✅ Converte tamanho (KB, MB, GB)
    ✅ Ordena do maior para o menor

---
+++ SQL_Server/Administrando_Bancos/Filegroup-Space.sql
@@ -0,0 +1,28 @@
+USE DBADMIN
+GO
+
+SELECT FileGroup = FILEGROUP_NAME(a.data_space_id),
+       SchemaName = OBJECT_SCHEMA_NAME(p.object_id),
+       TableName = OBJECT_NAME(p.object_id),
+       IndexName = i.name,
+       8 * SUM(a.used_pages) AS 'Size(KB)',
+       8 * SUM(a.used_pages) / 1024 AS 'Size(MB)',
+       8 * SUM(a.used_pages) / 1024 / 1024 AS 'Size(GB)'
+FROM sys.allocation_units a
+INNER JOIN sys.partitions p ON a.container_id = CASE
+                                                    WHEN a.type IN (1,
+                                                                    3) THEN p.hobt_id
+                                                    ELSE p.partition_id
+                                                END
+AND p.object_id > 1024
+LEFT JOIN sys.indexes i ON i.object_id = p.object_id
+AND i.index_id = p.index_id --Uncomment if you want to see indexes for one filegroup
+--WHERE
+--    a.data_space_id = 1
+GROUP BY a.data_space_id,
+         p.object_id,
+         i.object_id,
+         i.index_id,
+         i.name
+ORDER BY FILEGROUP_NAME(a.data_space_id),
+         SUM(a.used_pages) DESC;
